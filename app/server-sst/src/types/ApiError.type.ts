c078b04c30475c5dfb280bc94690e6879b397c81
kadaradam <xadam1996@gmail.com>
2024-11-03 16:12:46 +0100

    feat: add api handler: body validation, error handling

---
+++ app/server-sst/src/types/ApiError.type.ts
@@ -0,0 +1,16 @@
+export enum ApiErrorType {
+  // COMMON
+  NOT_FOUND = "not_found",
+  VALIDATION_ERROR = "validation_error",
+  UNEXPECTED_ERROR = "unexpected_error",
+  SYNTAX_ERROR = "syntax_error",
+  BAD_REQUEST = "bad_request",
+
+  // CORS
+  CORS_BLOCKED = "cors_blocked",
+}
+
+export type ErrorResponse = {
+  type: ApiErrorType;
+  message: string;
+};
