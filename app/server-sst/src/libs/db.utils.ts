59321cceb029cd5502dfe3b7dcf397e71ec14031
kadaradam <xadam1996@gmail.com>
2024-11-03 20:06:53 +0100

    feat: add custom dynamodb wrapper orm

---
+++ app/server-sst/src/libs/db.utils.ts
@@ -0,0 +1,57 @@
+import { AttributeValue } from "@aws-sdk/client-dynamodb";
+import { ItemProps } from "../types/db.type";
+
+export function convertToDynamoSchema<T>(
+  props: ItemProps<T>
+): Record<string, AttributeValue> {
+  return Object.entries(props).reduce((acc, [key, value]) => {
+    acc[key] =
+      typeof value === "string"
+        ? { S: value }
+        : typeof value === "boolean"
+        ? { BOOL: value }
+        : { N: (value as number).toString() };
+    return acc;
+  }, {} as Record<string, AttributeValue>);
+}
+
+export function convertToJavaScriptSchema(
+  dynamoObject: Record<string, AttributeValue>
+): Record<string, any> {
+  const obj: Record<string, any> = {};
+
+  Object.entries(dynamoObject).forEach(([key, value]) => {
+    if ("N" in value) {
+      obj[key] = Number(value.N);
+    } else if ("S" in value) {
+      obj[key] = value.S;
+    } else if ("BOOL" in value) {
+      obj[key] = value.BOOL;
+    }
+  });
+
+  return obj;
+}
+
+export function dbExpression<T>(
+  props: ItemProps<T>,
+  { update }: { update: boolean } = { update: false }
+): string {
+  const expression = Object.keys(props)
+    .map((key) => `${key} = :${key}`)
+    .join(", ");
+
+  if (update) {
+    return `SET ${expression}`;
+  }
+
+  return expression;
+}
+
+export function dbExpressionValues<T>(
+  props: ItemProps<T>
+): Record<string, any> {
+  return Object.fromEntries(
+    Object.entries(props).map(([key, value]) => [`:${key}`, value])
+  );
+}
---
+++ app/server-sst/src/types/db.type.ts
@@ -0,0 +1,6 @@
+export type WithoutId<T> = Omit<T, "id">;
+export type ItemProps<T> = Partial<Record<keyof T, number | string | boolean>>;
+export type NumberItemProp<T> = Extract<
+  keyof T,
+  { [K in keyof T]: T[K] extends number ? K : never }[keyof T]
+>;
